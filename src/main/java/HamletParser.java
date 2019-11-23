31f8d95df1b4da1c668b1dc0db65986097e2e7a1
Brian Patterson <brian@zipcoders-MacBook-Pro-5.local>
2019-11-22 19:42:31 -0500

    Completed Lab

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -9,30 +11,65 @@ public class HamletParser {
 
     private String hamletData;
 
-    public HamletParser(){
+
+
+    public HamletParser() {
         this.hamletData = loadFile();
     }
 
-    private String loadFile(){
-        ClassLoader classLoader = getClass().getClassLoader();
-        File file = new File(classLoader.getResource("hamlet.txt").getFile());
+    public String changeHoratio(String stringToParse) {
+        String regex1 = "Horatio";
+        Pattern pattern = Pattern.compile(regex1);
+        Matcher matcherHam = pattern.matcher(stringToParse);
+        String newStory = matcherHam.replaceAll("Tariq");
+
+
+        return newStory;
+    }
+
+    public String changeHamlet(String stringToParse) {
+        String regex = "Hamlet";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcherHam = pattern.matcher(stringToParse);
+        String newStory = matcherHam.replaceAll("Leon");
+        return newStory;
+
+    }
+
+    public Boolean findHamlet(String stringToParse){
+        String regex = "Hamlet";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcherHam = pattern.matcher(stringToParse);
+        return matcherHam.find();
+    }
+    public Boolean findHoratio(String stingToParse){
+        String regex2 = "Horatio";
+        Pattern pattern2 = Pattern.compile(regex2);
+        Matcher matcherHor = pattern2.matcher(stingToParse);
+        return matcherHor.find();
+    }
+
+    private String loadFile() {
+        //ClassLoader classLoader = getClass().getClassLoader();
+        File file = new File("/Users/brian/dev/Week 6/Maven.Regex-Hamlet-Parser/src/main/resources/hamlet.txt");
         StringBuilder result = new StringBuilder("");
 
-        try(Scanner scanner = new Scanner(file)){
-            while(scanner.hasNextLine()){
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
                 result.append(line).append("\n");
             }
 
             scanner.close();
-        }catch(IOException e){
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
         return result.toString();
     }
 
-    public String getHamletData(){
+
+    public String getHamletData() {
         return hamletData;
     }
 
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -1,5 +1,8 @@
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -14,18 +17,35 @@ public class HamletParserTest {
     }
 
     @Test
+    public void getHamletData() {
+        String actual = hamletParser.getHamletData();
+        String expected = hamletText;
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
     public void testChangeHamletToLeon() {
+        String tester = hamletParser.changeHamlet(hamletText);
+        Assert.assertFalse(hamletParser.findHamlet(tester));
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        String tester = hamletParser.changeHoratio(hamletText);
+        Assert.assertFalse(hamletParser.findHoratio(tester));
+
     }
 
     @Test
     public void testFindHoratio() {
+        Assert.assertTrue(hamletParser.findHoratio(hamletText));
+        Assert.assertFalse(hamletParser.findHoratio("Hopatio is a dude of some sort"));
     }
 
     @Test
     public void testFindHamlet() {
+        Assert.assertTrue(hamletParser.findHamlet(hamletText));
+        Assert.assertFalse(hamletParser.findHamlet("Hamjet is a dude of some sort"));
     }
 }
